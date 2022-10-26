66061ba21b667beaadab1cd30fdd27837cf8737b
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2022-10-26 12:06:12 -0700

    fixes

--- src/test/scala/simplequeue/SimpleQueue.scala
+++ src/test/scala/simplequeue/SimpleQueue.scala
@@ -6,18 +6,19 @@ import chisel3.util._
 import chisel3.experimental.BundleLiterals._
 import chisel3.experimental.{DataMirror, Direction}
 import scala.collection.mutable
-
+/*
 class Transaction(width: Int) extends Bundle { 
   assert(width <= 32, "The width of the data should be less than 32 bits for decoupled interface to work, but I don't know how to handle the case when the width of the data is more than 32bits and only pass in 32 bits of data.")
   val bits: UInt = UInt(width.W) 
 }
+*/
 
 class Transaction[T <: Data](gen: T) extends Bundle { val bits: T = gen }
 
 object QueueDriver {
   //a function that can drive a transaction 
   //into an enqueuing Decoupled interface
-  def drive(t: Transaction, interface: DecoupledIO[UInt], clock: Clock): Unit = {
+  def drive[T <: Data](t: Transaction[T], interface: DecoupledIO[T], clock: Clock): Unit = {
     assert(t.bits.litOption.isDefined)
     interface.valid.poke(true.B)
     interface.bits.poke(t.bits)
@@ -28,22 +29,25 @@ object QueueDriver {
     // at this point ready and valid are both true
     clock.step(1)
     interface.valid.poke(false.B)
-    interface.bits.poke(0.U)  //?? Now the queue only accept 1 value?
+    //interface.bits.poke(0.U)  //?? Now the queue only accept 1 value?
   }
 }
 
 object QueueReciever {
   // a function that can get the bits out from 
   // an enqueuing Decoupled interface to a transaction. 
-  def receive(interface: DecoupledIO[UInt], clock: Clock): Transaction = {
+  def receive[T <: Data](interface: DecoupledIO[T], clock: Clock, gen: T): Transaction[T] = {
+    println("receiving")
     interface.ready.poke(true.B)
     while (!interface.valid.peek().litToBoolean) {
       clock.step(1)
     }
-    val peeked = interface.bits.peek()
+    val peeked: T = interface.bits.peek()
     clock.step(1)
     interface.ready.poke(false.B)
-    val t = new Transaction(32)
-    t.Lit(b => b.bits -> peeked)
+    val t = new Transaction(gen)
+    val ret = t.Lit(b => b.bits -> peeked)
+    println(ret)
+    ret
   }
 }
--- src/test/scala/simplequeue/SimpleQueue_tb.scala
+++ src/test/scala/simplequeue/SimpleQueue_tb.scala
@@ -8,20 +8,29 @@ import org.scalatest.freespec.AnyFreeSpec
 import chiseltest.{TreadleBackendAnnotation, WriteVcdAnnotation}
 import chisel3.experimental.BundleLiterals._
 
+import scala.collection.mutable
+
 class QueueTest extends AnyFreeSpec with ChiselScalatestTester {
   "run a test" in {
-    test(new Queue(UInt(32.W), 8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+    val gen = UInt(32.W)
+    test(new Queue(gen, 8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
       c.clock.step(10)
       val txns = (0 until 4).map{ i =>
-        new Transaction(32).Lit(b => b.bits -> (100+i).U)
+        new Transaction(gen).Lit(b => b.bits -> (100+i).U)
       }
       txns.foreach { t=>
         QueueDriver.drive(t , c.io.enq, c.clock)
       }
       c.clock.step(5)
-      val recvTxns = (0 until 4).foldLeft(Seq[Transaction]()){ (seq, i) =>
-        seq :+ QueueReciever.receive(c.io.deq, c.clock)  // repeat 4 times with (0 until 4) and what is "i?
+      val recvTxns = mutable.ListBuffer[Transaction[UInt]]()
+      for (i <- 0 until 4) {
+        recvTxns.addOne(QueueReciever.receive(c.io.deq, c.clock, gen))  // repeat 4 times with (0 until 4) and what is "i?
       }
+      /*
+      val recvTxns = (0 until 4).foldLeft(Seq[Transaction[UInt]]()){ (seq, i) =>
+        seq :+ QueueReciever.receive(c.io.deq, c.clock, gen)  // repeat 4 times with (0 until 4) and what is "i?
+      }
+      */
       c.clock.step(1)
       println(recvTxns)
       c.clock.step(5)
@@ -30,25 +39,25 @@ class QueueTest extends AnyFreeSpec with ChiselScalatestTester {
     }
   }
 
-    "run a parallel test" in {
-    test(new Queue(UInt(32.W), 8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-    val txns = (0 until 10).map{ i =>
-        new Transaction(32).Lit(_.bits -> (i).U)
+  "run a parallel test" in {
+    val gen = UInt(32.W)
+    test(new Queue(gen, 8)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      val txns = (0 until 10).map{ i =>
+        new Transaction(gen).Lit(_.bits -> (i).U)
       }
-    val seq:Seq[Transaction] = Seq()  
-      fork{
-        txns.foreach { t=>
-        QueueDriver.drive(t , c.io.enq, c.clock)
-        c.clock.step(1)
-      }
+      val recvTxns = mutable.ListBuffer[Transaction[UInt]]()
+      fork {
+        txns.foreach { t =>
+          QueueDriver.drive(t, c.io.enq, c.clock)
+          c.clock.step(1)
+        }
       }.fork{
-        c.clock.step(2)
-        //val sseq = 
-        //val sseq +: t
-        val recvTxns = (0 until 4).foldLeft(Seq[Transaction]()){ (seq, i) =>
-        seq :+ QueueReciever.receive(c.io.deq, c.clock)
+        c.clock.step(200)
+        for (i <- (0 until 10)) {
+          recvTxns.addOne(QueueReciever.receive(c.io.deq, c.clock, gen))
         }
       }.join()
+      println(recvTxns)
     }
   }
 }
