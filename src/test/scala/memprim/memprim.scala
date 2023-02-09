ec41e06bad270c1f4f5a03ef97e0e169f766eed8
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2023-02-08 17:08:48 -0800

    basic function structure for memprim conversion

--- src/test/scala/memprim/memprim.scala
+++ src/test/scala/memprim/memprim.scala
@@ -22,16 +22,47 @@ sealed trait MemPrim {
     }
 }
 
+case class TLBundleParameters(
+  addressBits: Int,
+  dataBits:    Int,
+  sourceBits:  Int,
+  sinkBits:    Int,
+  sizeBits:    Int)
+
+object MemPrim {
+    def translateToTL(mp: MemPrim, tlBundleParams: TLBundleParameters): Seq[TLPrim] = {
+        mp match {
+            case Read(addr, nBytes) =>
+                if (nBytes == tlBundleParams.dataBits*8) {
+                    Get(addr, tlBundleParams.dataBits)
+                } else {
+                    // This is a Get + another (or more) Get
+                    ???
+                }
+            case Write(addr, data: Seq[Byte]) =>
+                // TODO: this is wrong - the types don't match up
+                if (data.length*8 == dataBits) {
+                    PutFullData(data.toByteArray(), addr, tlBundleParams.dataBits)
+                } else if (data.length*8 < dataBits) {
+                    ??? // this must be a PutPartial
+                } else { // we have more data than can fit on one bus beat
+                    ??? // this must be a PutFullData + Seq[PutFullData] + PutPartialData
+                }
+        }
+    }
+}
+
 case class Channel(channels: Map[String, Seq[Byte]]) extends collection.mutable.Map
 // map("A") = Seq[Byte]
 // map("B") = Seq[Byte]
 // map("C") = Seq[Byte]
 // map("D") = Seq[Byte]
 // map("E") = Seq[Byte]
-case class Read(addr: BigInt) extends MemPrim
-case class ReadResponse(data: Seq[Byte]) extends MemPrim
+case class Read(addr: BigInt, nBytes: Int) extends MemPrim
+//case class ReadResponse(data: Seq[Byte]) extends MemPrim
 case class Write(addr: BigInt, data: Seq[Byte]) extends MemPrim
 
 sealed abstract class TLPrim(dataWidth: Int)
-case class Get(addr: BigInt, corrupt) extends TLPrim
-case class Put(data: BigInt, addr: BitInt, corrupt) extends TLPrim
+case class Get(addr: BigInt, dataWidth: Int) extends TLPrim(dataWidth)
+case class PutFullData(data: BigInt, addr: BitInt, dataWidth: Int) extends TLPrim(dataWidth)
+case class PutPartialData(data: BigInt, addr: BitInt, dataWidth: Int) extends TLPrim(dataWidth)
